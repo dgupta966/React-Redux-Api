b9d04a60f8e4af4afb21f0e77f005110b55bd61b
Deepak Gupta <88889634+dgupta966@users.noreply.github.com>
2021-10-22 12:00:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Items from "./components/items";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Fake Api </h1>
+      <Items />
+    </div>
+  );
+}
