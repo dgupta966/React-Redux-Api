f63b287b1c7c23a93077b52384a724c46abfbf63
Deepak Gupta <88889634+dgupta966@users.noreply.github.com>
2021-10-22 12:09:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Fake Api </h1>
+      <h4>React-Redux-Axios</h4>
       <Items />
       <div style={{color:"blue"}}>@deepakgupta</div>
     </div>
